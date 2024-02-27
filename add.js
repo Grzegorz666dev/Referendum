9dbde9b87720620a5010a9b08e976c3e42d257b4
Grzegorz666dev <grzegorz666dev@gmail.com>
2024-02-27 21:26:10 +0100

    copy from another file

---
+++ add.js
@@ -0,0 +1,19 @@
+function updateVotesCount() {
+    fetch('http://localhost:3000/getVotes')
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                const votesForTak = data.votesForTak || 0;
+                const votesForNie = data.votesForNie || 0;
+
+                document.getElementById('votesCountTak').textContent = `Ilość na tak: ${votesForTak}`;
+                document.getElementById('votesCountNie').textContent = `Ilość na nie: ${votesForNie}`;
+            } else {
+                console.error('Błąd pobierania ilości głosów:', data.error);
+            }
+        })
+        .catch(error => console.error('Błąd:', error));
+}
+
+window.onload = updateVotesCount;
+
